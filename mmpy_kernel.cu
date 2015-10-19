5edf309a0d45a1be93b0a00d736918bcecd4e7ee
Honam Bang <hobang@ucsd.edu>
2015-10-19 09:47:36 -0700

    Acheived 270 Gigaflops

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -1,22 +1,82 @@
 // Matrix multiply device code
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
 #include "utils.h"
 #include "types.h"
 using namespace std;
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int wA = N;
+    int wB = N;
+    const int BLOCK_SIZE = blockIdx.x; //square
 
-    if((I < N) && (J < N)){
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
+    // Index of the first sub-matrix of A processed by the block
+    int aBegin = wA * BLOCK_SIZE * by;
+
+    // Index of the last sub-matrix of A processed by the block
+    int aEnd   = aBegin + wA - 1;
+
+    // Step size used to iterate through the sub-matrices of A
+    int aStep  = BLOCK_SIZE;
+
+    // Index of the first sub-matrix of B processed by the block
+    int bBegin = BLOCK_SIZE * bx;
+
+    // Step size used to iterate through the sub-matrices of B
+    int bStep  = BLOCK_SIZE * wB;
+
+    // Csub is used to store the element of the block sub-matrix
+    // that is computed by the thread
+    _DOUBLE_ Csub = 0;
+
+    // Loop over all the sub-matrices of A and B
+    // required to compute the block sub-matrix
+    for (int a = aBegin, b = bBegin;
+         a <= aEnd;
+         a += aStep, b += bStep)
+    {
+
+        // Declaration of the shared memory array As used to
+        // store the sub-matrix of A
+        __shared__ _DOUBLE_ As[BLOCK_SIZE][BLOCK_SIZE];
+
+        // Declaration of the shared memory array Bs used to
+        // store the sub-matrix of B
+        __shared__ _DOUBLE_ Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+        // Load the matrices from device memory
+        // to shared memory; each thread loads
+        // one element of each matrix
+        As[ty][tx] = A[a + wA * ty + tx];
+        Bs[ty][tx] = B[b + wB * ty + tx];
+
+        // Synchronize to make sure the matrices are loaded
+        __syncthreads();
+
+        // Multiply the two matrices together;
+        // each thread computes one element
+        // of the block sub-matrix
+#pragma unroll
+
+        for (int k = 0; k < BLOCK_SIZE; ++k)
+        {
+            Csub += As[ty][k] * Bs[k][tx];
         }
-        C[I * N + J] = _c;
+
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
     }
+
+    // Write the block sub-matrix to device memory;
+    // each thread writes one element
+    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+    C[c + wB * ty + tx] = Csub;
 }

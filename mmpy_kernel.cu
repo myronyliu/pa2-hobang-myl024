3f558f9214a5d298dd31c3c103e5be2fd62f57a3
Myron Liu <myronyliu@gmail.com>
2015-10-26 17:39:55 -0700

    matMul now handles matrices of arbitrary size. The conditional statements slow things down though

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -5,14 +5,22 @@
 #include "utils.h"
 #include "types.h"
 using namespace std;
+
 #define BLOCK_SIZE 32
 
-__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
+__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
+{
     int bx = blockIdx.x;
     int by = blockIdx.y;
 
     int tx = threadIdx.x;
     int ty = threadIdx.y;
+
+	int x = bx*BLOCK_SIZE + tx;
+	int y = by*BLOCK_SIZE + ty;
+
+	//if (x >= N || y >= N) return;
+
     int wA = N;
     int wB = N;
 
@@ -24,28 +32,46 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 	
 	__shared__ _DOUBLE_ As[BLOCK_SIZE][BLOCK_SIZE];
     __shared__ _DOUBLE_ Bs[BLOCK_SIZE][BLOCK_SIZE];
-    _DOUBLE_ Csub[2] = {0,0};
+
+    _DOUBLE_ Csub[2] = {0, 0};
 
     for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep)
     {
-		As[ty][tx] = A[a + wA * ty + tx];
-        Bs[ty][tx] = B[b + wB * ty + tx];
+		As[ty     ][tx] = A[a + wA *  ty       + tx];
+        Bs[ty     ][tx] = B[b + wB *  ty       + tx];
 		As[ty + 16][tx] = A[a + wA * (ty + 16) + tx];
  		Bs[ty + 16][tx] = B[b + wB * (ty + 16) + tx];
 
         __syncthreads();
 
-		#pragma unroll
-        for (int k = 0; k < BLOCK_SIZE; ++k)
-        {
-            Csub[0] += As[ty][k] * Bs[k][tx];
-			Csub[1] += As[ty + 16][k] * Bs[k][tx];
-        }
+		if (x < N && y < N)
+		{
+			int w = N - (a - aBegin);
+			int kMax = (w < BLOCK_SIZE) ? w : BLOCK_SIZE;
 
+			#pragma unroll
+			for (int k = 0; k < kMax; ++k)
+			{
+				Csub[0] += As[ty][k] * Bs[k][tx];
+
+				if (y + 16 < N)
+				{
+					Csub[1] += As[ty + 16][k] * Bs[k][tx];
+				}
+			}
+		}
         __syncthreads();
     }
 
-    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-    C[c + wB * ty + tx] = Csub[0];
-	C[c + wB * (ty + 16) + tx] = Csub[1];
+	if (x < N)
+	{
+		int c = wB*BLOCK_SIZE*by + BLOCK_SIZE*bx;
+
+		C[c + wB*ty + tx] = Csub[0];
+
+		if (y + 16 < N)
+		{
+			C[c + wB*(ty + 16) + tx] = Csub[1];
+		}
+	}
 }

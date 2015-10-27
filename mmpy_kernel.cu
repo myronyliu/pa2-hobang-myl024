2406d514f39e9304985924795e73c8bf768e1cd7
Myron Liu <myronyliu@gmail.com>
2015-10-27 13:51:53 -0700

    fixing incorrect algorithm

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -7,28 +7,26 @@
 using namespace std;
 
 // The effective block size, in actuality we only have BLOCK_H threads
-#define BLOCK_W 64
-#define BLOCK_H 16
+#define BLOCK_W 16 
+#define BLOCK_H 64 
 
-// the A-block is square with dimensions: BLOCK_H x BLOCK_H
-// the B-block and C-block are rectangular with dimensions: BLOCK_H x BLOCK_W
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 {
 	int jBlock = blockIdx.x;
 	int iBlock = blockIdx.y;
 
-	int iThread = threadIdx.x; // each block's has BLOCK_H threads (one for each row of the C-block)
+	int iThread = threadIdx.x; // each block's has BLOCK_W threads (one for each column of the C-block)
 
-	int AsBegin = N * BLOCK_H * iBlock;
+	int AsBegin = (N * BLOCK_H) * iBlock;
 	int AsStep  = BLOCK_H;
 
 	int BsBegin = BLOCK_W * jBlock;
 	int BsStep  = BLOCK_H * N;
 
-	__shared__ _DOUBLE_ As[BLOCK_H][BLOCK_H]; // 'As' holds the A-block 
+	__shared__ _DOUBLE_ Bs[BLOCK_W][BLOCK_W]; // 'Bs' holds the shared, parallel-loaded, B-block 
 
-	_DOUBLE_ b[BLOCK_W]; // 'b' holds a row of the B-block
-	_DOUBLE_ c[BLOCK_W]; // we compute a row of the C-block and put the result in 'c' temporarily
+	_DOUBLE_ a; // the BLOCK_H a's (one for each thread), together hold a column of the A-block
+	_DOUBLE_ c[BLOCK_W]; // the BLOCK_H c's (one for each thread) accumulate into a block of C
 
 	#pragma unroll
 	for (int j = 0; j < BLOCK_W; ++j) // initialize c[] to a row vector of all zeroes
@@ -41,30 +39,25 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 	//////////////////////////////////////
 
 	for (	int AsIndex = AsBegin, BsIndex = BsBegin;
-			AsIndex <= AsBegin + N - 1;
+			AsIndex < AsBegin + N;
 			AsIndex += AsStep, BsIndex += BsStep
 		)
 	{
-		// Load a '1 by BLOCK_W' vector of the B-block 
-		// Load a 'BLOCK_H by BLOCK_H' block of A 
-		// These access global memory, so we want successive j's to correspond to contiguous memory
-		for (int j = 0; j < BLOCK_W; ++j)
+		for (int j = 0; j < BLOCK_W; ++j) // load shared B block in parallel
 		{
-			b[j] = B[BsIndex + N * iThread + j];
-		}
-		for (int j = 0; j < BLOCK_H; ++j)
-		{
-			As[j][iThread] = A[AsIndex + N * iThread + j]; // transpose to avoid 'bank conflict'
+			Bs[j][iThread] = B[BsIndex + N*iThread + j]; // transpose to avoid 'bank conflict'
 		}
 		__syncthreads();
 
 		#pragma unroll
-		for (int j = 0; j < BLOCK_H; ++j) // for each element in a row of As
+		for (int j = 0; j < BLOCK_W; ++j) // ... for each column of the A-block
 		{
+			a = A[AsIndex + N*iThread + j];
+
 			#pragma unroll
-			for (int jj = 0; jj < BLOCK_W; ++jj)
+			for (int jj = 0; jj < BLOCK_W; ++jj) // accumulate a*Bs[:][iThread] into c[:]
 			{
-				c[jj] += As[j][iThread] * b[jj];
+				c[jj] += a * Bs[jj][iThread];
 			}
 		}
 		__syncthreads();

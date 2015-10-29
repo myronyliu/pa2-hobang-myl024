e2ed89e62712023031b4c340e01d9eee77488b0d
Myron Liu <myronyliu@gmail.com>
2015-10-29 01:54:18 -0700

    going to try to change to shared A-block in an attempt to resolve bank conflicts

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -9,7 +9,7 @@ using namespace std;
 // The effective block size, in actuality we only have BLOCK_H threads
 
 #define BLOCK_W 16 
-#define BLOCK_H 64 
+#define BLOCK_H 64
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 {
@@ -24,7 +24,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 	int BsBegin = BLOCK_W * jBlock;
 	int BsStep  = BLOCK_W * N;
 
-	__shared__ _DOUBLE_ Bs[BLOCK_W][BLOCK_W]; // 'Bs' holds the shared, parallel-loaded, B-block 
+	__shared__ _DOUBLE_ Bs[BLOCK_W][BLOCK_W + 1]; // 'Bs' holds the shared, parallel-loaded, B-block 
 
 	_DOUBLE_ a; // the BLOCK_H a's (one for each thread), together hold a column of the A-block
 	_DOUBLE_ c[BLOCK_W] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -40,10 +40,10 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 		int Bi = iThread / BLOCK_W;
 		int Bj = iThread % BLOCK_W;
 
-		Bs[Bi     ][Bj] = B[BsIndex + N* Bi       + Bj];
-		Bs[Bi +  4][Bj] = B[BsIndex + N*(Bi +  4) + Bj];
-		Bs[Bi +  8][Bj] = B[BsIndex + N*(Bi +  8) + Bj];
-		Bs[Bi + 12][Bj] = B[BsIndex + N*(Bi + 12) + Bj];
+		Bs[Bj][Bi     ] = B[BsIndex + N* Bi       + Bj];
+		Bs[Bj][Bi +  4] = B[BsIndex + N*(Bi +  4) + Bj];
+		Bs[Bj][Bi +  8] = B[BsIndex + N*(Bi +  8) + Bj];
+		Bs[Bj][Bi + 12] = B[BsIndex + N*(Bi + 12) + Bj];
 
 		__syncthreads();
 
@@ -52,11 +52,29 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 		{
 			a = A[AsIndex + N*iThread + k];
 
+			c[0] += a * Bs[0][k];
+			c[1] += a * Bs[1][k];
+			c[2] += a * Bs[2][k];
+			c[3] += a * Bs[3][k];
+			c[4] += a * Bs[4][k];
+			c[5] += a * Bs[5][k];
+			c[6] += a * Bs[6][k];
+			c[7] += a * Bs[7][k];
+			c[8] += a * Bs[8][k];
+			c[9] += a * Bs[9][k];
+			c[10] += a * Bs[10][k];
+			c[11] += a * Bs[11][k];
+			c[12] += a * Bs[12][k];
+			c[13] += a * Bs[13][k];
+			c[14] += a * Bs[14][k];
+			c[15] += a * Bs[15][k];
+/*
 			#pragma unroll
 			for (int j = 0; j < BLOCK_W; ++j)
 			{
 				c[j] += a * Bs[k][j];
 			}
+*/
 		}
 		__syncthreads();
 	}
